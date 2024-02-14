0418e3fb7718c03f02659f3065622eaad572df37
JuanOrtiz2006 <150272778+JuanOrtiz2006@users.noreply.github.com>
2024-02-14 11:13:58 -0500

    Add files via upload

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,28 +7,27 @@ import { Firestore, doc, setDoc } from '@angular/fire/firestore';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  led: boolean = false;//Control de boton 1
-  led2: boolean = false;//Control de boton 2
-  led3: boolean = false;//Control de boton 3
-  led4: boolean = false;//Control de boton 4
-  buttonColor: string = '';//Cambiar color de boton 1
-  buttonColor2: string = '';//Cambiar color de boton 2
-  buttonColor3: string = '';//Cambiar color de boton 3
-  buttonColor4: string = '';//Cambiar color de boton 4
+  led: boolean = false;
+  led2: boolean = false;
+  led3: boolean = false;
+  led4: boolean = false;
+  buttonColor: string = '';
+  buttonColor2: string = '';
+  buttonColor3: string = '';
+  buttonColor4: string = '';
 
   constructor(private db: Firestore) {}
-  //Metodo para cambiar de color los botones y cambiar el valor del dato en firestore
-  async mixto(buttonNumber: number)//El metodo tiene una variable tipo numero
-  {
-    if (buttonNumber == 1) {//Si la variable es igual a 1
-      this.led = !this.led;//El valor del led cambia
-      this.buttonColor = this.led ? 'success' : 'secondary';//Cambia el color del boton
-      await setDoc(doc(this.db, 'controlLed/controlLed'), { encender: this.led });//Cambia el valor en el firestore
-    } else if (buttonNumber == 2) {
+
+  async mixto(buttonNumber: number) {
+    if (buttonNumber === 1) {
+      this.led = !this.led;
+      this.buttonColor = this.led ? 'success' : 'secondary';
+      await setDoc(doc(this.db, 'controlLed/controlLed'), { encender: this.led });
+    } else if (buttonNumber === 2) {
       this.led2 = !this.led2;
       this.buttonColor2 = this.led2 ? 'success' : 'secondary';
       await setDoc(doc(this.db, 'controlLed/controlLed2'), { encender: this.led2 });
-    } else if (buttonNumber == 3) {
+    } else if (buttonNumber === 3) {
       this.led3 = !this.led3;
       this.buttonColor3 = this.led3 ? 'success' : 'secondary';
       await setDoc(doc(this.db, 'controlLed/controlLed3'), { encender: this.led3 });
@@ -37,12 +36,12 @@ export class HomePage {
 
   async todos() {
     this.led4 = !this.led4;
-    this.led = !this.led;
-    this.led2 = !this.led2;
-    this.led3 = !this.led3;
     this.buttonColor = this.led4 ? 'success' : 'secondary';
     this.buttonColor2 = this.led4 ? 'success' : 'secondary';
     this.buttonColor3 = this.led4 ? 'success' : 'secondary';
-    this.buttonColor4 = this.led4 ? 'success' : 'secondary';
-    }
+    await setDoc(doc(this.db, 'controlLed/controlLed'), { encender: this.led4 });
+    await setDoc(doc(this.db, 'controlLed/controlLed2'), { encender: this.led4 });
+    await setDoc(doc(this.db, 'controlLed/controlLed3'), { encender: this.led4 });
+  }
 }
+
